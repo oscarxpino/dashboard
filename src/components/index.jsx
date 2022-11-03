c04883c679c5d75ba170133f40e2b28a7dd70f29
Oscar <oscar.pino17@tectijuana.edu.mx>
2022-11-03 12:34:34 -0700

    update

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,3 @@
-
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
 export { default as Sidebar } from './Sidebar';
@@ -14,3 +13,4 @@ export { default as Stacked } from './Charts/Stacked';
 export { default as Pie } from './Charts/Pie';
 export { default as ChartsHeader } from './ChartsHeader';
 export { default as Header } from './Header';
+
